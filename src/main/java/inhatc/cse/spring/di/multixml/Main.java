6e068b1951acd98e014ed582d09d20743785ac55
Haejeong079 <127839004+Haejeong079@users.noreply.github.com>
2024-03-26 18:37:59 +0900

    3.26
    add family, config

--- src/main/java/inhatc/cse/spring/di/multixml/Main.java
+++ src/main/java/inhatc/cse/spring/di/multixml/Main.java
@@ -6,10 +6,15 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         String location = "classpath:multi_xml/appCTX.xml";
+        String location2 = "classpath:multi_xml/app2CTX.xml";
 
-        AbstractApplicationContext ctx = new GenericXmlApplicationContext(location);
+        AbstractApplicationContext ctx = new GenericXmlApplicationContext(location,location2);
         StudentInfo studentInfo = ctx.getBean("studentInfo",StudentInfo.class);
         System.out.println(studentInfo.getStudent().getName());
+
+        Family family = ctx.getBean("family",Family.class);
+        System.out.println(family.getPapaName());
+        System.out.println(family.getSisterName());
 
     }
 }
