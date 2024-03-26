7f08e78d542832cd4a8faf639548a25291f539c5
Koseohyeon <ksh030818@naver.com>
2024-03-26 14:14:56 +0900

    저번주 수업
    수정

--- src/main/java/inhatc/cse/spring/di/properties/MainConstructor.java
+++ src/main/java/inhatc/cse/spring/di/properties/MainConstructor.java
@@ -5,11 +5,13 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class MainConstructor {
     public static void main(String[] args) {
-        String location="classpath:properties/constructorCTX.xml";
-        AbstractApplicationContext ctx= new GenericXmlApplicationContext(location);
-        MyInfo myInfo = ctx.getBean("myInfo", MyInfo.class);
+        String location = "classpath:properties/constructorCTX.xml";
 
-
-
+        try (AbstractApplicationContext ctx = new GenericXmlApplicationContext(location)) {
+            MyInfo myInfo = ctx.getBean("myInfo", MyInfo.class);
+            System.out.println("키: " + myInfo.getHeight());
+            System.out.println("체중: " + myInfo.getWeight());
+            myInfo.getBmiCalc().bmiCalc(myInfo.getWeight(), myInfo.getHeight());
+        }
     }
 }
